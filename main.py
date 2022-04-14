4f4bf64b182173aa8197de8fd922f83f1b1a1705
theo laptop <theo.badoz@gmail.com>
2022-04-14 16:10:14 +0200

    'i forgot what i changed'

--- main.py
+++ main.py
@@ -9,7 +9,41 @@ import data_analysis as analysis
 st.set_page_config(page_icon="⚙️", page_title="LoLStats Dashboard", layout="wide")
 
 st.title('LolStats')
-st.markdown('Welcome to LolStats, an interactive webapp for competitive league of legends data exploration')
+st.markdown('Welcome to LolStats, an interactive webapp for competitive league of legends data exploration')         
+
+
+# TABS (encore un peu sombre pour moi)
+st.markdown(
+    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">',
+    unsafe_allow_html=True,
+)
+query_params = st.experimental_get_query_params()
+tabs = ["Position analysis", "Team analysis", "Player visualization"]
+if "tab" in query_params:
+    active_tab = query_params["tab"][0]
+else:
+    active_tab = "Position analysis"
+
+if active_tab not in tabs:
+    st.experimental_set_query_params(tab="Position analysis")
+    active_tab = "Position analysis"
+
+li_items = "".join(
+    f"""
+    <li class="nav-item">
+        <a class="nav-link{' active' if t==active_tab else ''}" href="/?tab={t}">{t}</a>
+    </li>
+    """
+    for t in tabs
+)
+tabs_html = f"""
+    <ul class="nav nav-tabs">
+    {li_items}
+    </ul>
+"""
+
+st.markdown(tabs_html, unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
 
 
 def main_position():
@@ -60,41 +94,6 @@ def main_team():
             with st.expander('+ Pick analysis for the ' + league + ' league'):
                 analysis.winrate_side_per_league(league, top_teams_per_league, stats)
 
-        
-            
-
-
-st.markdown(
-    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">',
-    unsafe_allow_html=True,
-)
-query_params = st.experimental_get_query_params()
-tabs = ["Position analysis", "Team analysis", "Player visualization"]
-if "tab" in query_params:
-    active_tab = query_params["tab"][0]
-else:
-    active_tab = "Position analysis"
-
-if active_tab not in tabs:
-    st.experimental_set_query_params(tab="Position analysis")
-    active_tab = "Position analysis"
-
-li_items = "".join(
-    f"""
-    <li class="nav-item">
-        <a class="nav-link{' active' if t==active_tab else ''}" href="/?tab={t}">{t}</a>
-    </li>
-    """
-    for t in tabs
-)
-tabs_html = f"""
-    <ul class="nav nav-tabs">
-    {li_items}
-    </ul>
-"""
-
-st.markdown(tabs_html, unsafe_allow_html=True)
-st.markdown("<br>", unsafe_allow_html=True)
 
 if active_tab == "Position analysis":
     main_position()
@@ -104,6 +103,3 @@ elif active_tab == "Player visualization":
     st.write("If you'd like to contact me, then please don't.")
 else:
     st.error("Something has gone terribly wrong.")
-
-#if __name__ == "__main__":
-    #main()
